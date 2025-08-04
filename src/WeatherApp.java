fd749d1df1a9a9b63ce78cdf5b14f27d31d02100
Lyle Christine <cxz@gmx.com>
2025-08-04 01:57:47 +0100

    WeatherApp class added

--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -16,13 +16,72 @@ public class WeatherApp {
         // get location coordinates using the geolocation API
         JSONArray locationData = getLocationData(locationName);
 
+        // extract longitude and latitude data
+        JSONObject location = (JSONObject) locationData.get(0);
+        double longitude = (double) location.get("longitude");
+        double latitude = (double) location.get("latitude");
+
+        // build API request URL with location coordinates
+        String urlString = "https://api.open-meteo.com/v1/forecast?" +
+                "latitude=" + latitude + "&longitude=" + longitude +
+                "&hourly=temperature_2m,weather_code,wind_speed_10m,relative_humidity_2m&timezone=Europe%2FLondon&wind_speed_unit=mph";
+
+        try {
+            // call api can get response
+            HttpURLConnection conn = fetchApiResponse(urlString);
+
+            // check for response status
+
+            // 200 means the connection was a success
+            if(conn.getResponseCode() != 200){
+                System.out.println("Error: could not connect to API");
+                return null;
+            }
+
+            // store resulting JSON data
+            StringBuilder resultJson = new StringBuilder();
+            Scanner scanner = new Scanner(conn.getInputStream());
+
+            // read and store the resulting JSON data into our StringBuilder
+            while(scanner.hasNext()){
+                // read and store into the StringBuilder
+                resultJson.append(scanner.nextLine());
+            }
+
+            // close scanner
+            scanner.close();
+
+            // close URL connection
+            conn.disconnect();
+
+            // parse through our JSON data
+            JSONParser parser = new JSONParser();
+            JSONObject resultJsonObj = (JSONObject) parser.parse(String.valueOf(resultJson));
+
+            // retrieve hourly data
+            JSONObject hourly = (JSONObject) resultJsonObj.get("hourly");
+
+            // we want to get the curent hour's data
+            // so we need to get the index of the current hour
+            JSONArray time = (JSONArray) hourly.get("time");
+            int index = findIndexofCurrentHour(time);
+
+
+
+            // parse JSON data
+            JSONParser parser = new JSONParser();
+            return (JSONObject) parser.parse(resultJson.toString());
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         return null;
     }
 
     //retrieves geographic coordinates for a location name
     public static JSONArray getLocationData(String locationName) {
         // replace any whitespace in the location name with a plus sign to adhere to the Meteomatics API format
-        // e.g. "San Francisco" ->  https://open-meteo.com/en/docs/geocoding-api#name=San+Francisco#
+        // e.g. "New York ->  https://open-meteo.com/en/docs/geocoding-api#name=New+York#
         locationName = locationName.replaceAll("\\s", "+");
 
         // build API url with location name
